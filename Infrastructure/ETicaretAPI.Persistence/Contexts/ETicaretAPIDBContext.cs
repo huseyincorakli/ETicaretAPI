e99121e61ee100679217f19ac95cce4500ec3991
huseyin corakli <huseyincorakli@icloud.com>
2023-01-30 16:10:10 +0300

    SaveChangeAsync Interceptor

--- Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDBContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Domain;
+using ETicaretAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,5 +16,20 @@ namespace ETicaretAPI.Persistence.Contexts
         public DbSet<Product> Products{ get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers{ get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //SAVECHANGES INTERCEPTOR
+            var datas = ChangeTracker.Entries<BaseEntity>();
+            foreach (var item in datas)
+            {
+                _ = item.State switch
+                {
+                    EntityState.Added => item.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified=>item.Entity.UpdatedDate=DateTime.UtcNow,
+                };
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
--- Presentation/ETicaretAPI.API/Controllers/ProductController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -12,11 +12,25 @@ namespace ETicaretAPI.API.Controllers
     {
         readonly private IProductWriteRepository _productWriteRepository;
         readonly private IProductReadRepository _productReadRepository;
+        readonly private ICustomerReadRepository _customerReadRepository;
+        readonly private ICustomerWriteRepository _customerWriteRepository;
+        readonly private IOrderReadRepository _orderReadRepository;
+        readonly private IOrderWriteRepository _orderWriteRepository;
 
-        public ProductController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        public ProductController(
+            IProductWriteRepository productWriteRepository,
+            IProductReadRepository productReadRepository,
+            ICustomerReadRepository customerReadRepository,
+            ICustomerWriteRepository customerWriteRepository,
+            IOrderReadRepository orderReadRepository,
+            IOrderWriteRepository orderWriteRepository)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
+            _customerReadRepository = customerReadRepository;
+            _customerWriteRepository = customerWriteRepository;
+            _orderReadRepository = orderReadRepository;
+            _orderWriteRepository = orderWriteRepository;
         }
         [HttpGet]
         public async Task Get()
@@ -28,15 +42,34 @@ namespace ETicaretAPI.API.Controllers
             //    new() {  Id=Guid.NewGuid() ,Name="Product 4", CreatedDate=DateTime.UtcNow,Price=400,Stock=400 },
             //});
             //await _productWriteRepository.SaveAsync();
-            Product p = await _productReadRepository.GetByIdAsync("a31dfab0-9d2f-470b-8bcc-92190eb2f880");
-            p.Name = "Product 5";
-            await _productWriteRepository.SaveAsync();
+            //Product p = await _productReadRepository.GetByIdAsync("a31dfab0-9d2f-470b-8bcc-92190eb2f880");
+            //p.Name = "Product 5";
+            //await _productWriteRepository.SaveAsync();
+
+
+            ////*******new customer*********
+            //var id= Guid.NewGuid();
+            //await _customerWriteRepository.AddAsync(new() { Id=id ,Name="Hüseyin Çoraklı",});
+
+            ////******* new orders*************
+            //await _orderWriteRepository.AddAsync(new() { Address = "Kahramanmaraş-Dulkadiroğlu",CustomerId=id, Description="deneme1" });
+            //await _orderWriteRepository.AddAsync(new() { Address = "Kahramanmaraş-Onikişubat",CustomerId=id, Description="deneme2" });
+
+            //await _orderWriteRepository.SaveAsync();
+
+            Order order= await _orderReadRepository.GetByIdAsync("d6d48626-e24d-4933-b757-38d0458a577f");
+            order.Address = "Kahramanmaraş-Doğukent";
+            await _orderWriteRepository.SaveAsync();
+
         }
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(string id)
-        {
-           Product product = await _productReadRepository.GetByIdAsync(id);
-            return Ok(product);
-        }
+
+
+
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> Get(string id)
+        //{
+        //   Product product = await _productReadRepository.GetByIdAsync(id);
+        //    return Ok(product);
+        //}
     }
 }
